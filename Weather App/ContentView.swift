1a689140b019c32b3e1b2280feb61cbcceee9827
John Guerrero <ingjohnguerrero@gmail.com>
2025-06-15 11:26:31 -0500

    Initial Commit

---
+++ Weather App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather App
+//
+//  Created by John Guerrero on 6/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
